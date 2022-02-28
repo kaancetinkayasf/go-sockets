12bc43b07371e7f37bfde71cbcbf18aef3fdc06d
berke <kaancetinkayasf@gmail.com>
2022-02-28 12:31:54 +0300

    communicating via custom protocol

--- client/main.go
+++ client/main.go
@@ -1,8 +1,16 @@
 package main
 
 import (
+	"bufio"
+	"encoding/binary"
 	"fmt"
 	"net"
+	"os"
+)
+
+const (
+	TextMessage = 1
+	JsonMessage = 2
 )
 
 func main() {
@@ -10,6 +18,8 @@ func main() {
 	ConnectToServer()
 
 }
+
+var messageBuf []byte
 
 func ConnectToServer() {
 	connection, err := net.Dial("tcp4", "127.0.0.1:8080")
@@ -25,26 +35,61 @@ func handleConnection(connection net.Conn) {
 
 	go func() {
 		for {
-			buffer := make([]byte, 8096)
-			_, err := connection.Read(buffer[:])
+			header := make([]byte, 8)
+			_, err := connection.Read(header[:])
 			if err != nil {
 				fmt.Println(err)
 				connection.Close()
 				break
 			}
 
-			fmt.Printf("%s\n", buffer)
+			mlen := binary.LittleEndian.Uint32(header[4:])
+			dataBuffer := make([]byte, mlen)
+			_, err = connection.Read(dataBuffer[:])
+			if err != nil {
+				fmt.Println(err)
+				connection.Close()
+				break
+			}
+
+			messageBuf = append(messageBuf, header...)
+			messageBuf = append(messageBuf, dataBuffer...)
+
+			mtype, mlen, msg := readMessage(messageBuf)
+			messageBuf = nil
+			fmt.Printf("%d %d %s\n", mtype, mlen, msg)
 
 		}
 	}()
 
 	for {
-		buffer := make([]byte, 8096)
-		fmt.Scanf("%s", &buffer)
-		_, err := connection.Write(buffer)
+
+		consoleReader := bufio.NewReader(os.Stdin)
+		input, _ := consoleReader.ReadString('\n')
+
+		msg := createMessage(TextMessage, input)
+		_, err := connection.Write(msg)
 		if err != nil {
 			fmt.Println(err)
 		}
 
 	}
+}
+
+func createMessage(mtype int, data string) []byte {
+	buf := make([]byte, 4+4+len(data))
+	binary.LittleEndian.PutUint32(buf[0:], uint32(mtype))
+	binary.LittleEndian.PutUint32(buf[4:], uint32(len(data)))
+	copy(buf[8:], []byte(data))
+
+	return buf
+
+}
+
+func readMessage(data []byte) (mtype, mlen uint32, msg string) {
+	mtype = binary.LittleEndian.Uint32(data[0:])
+	mlen = binary.LittleEndian.Uint32(data[4:])
+	msg = string(data[8:])
+
+	return mtype, mlen, msg
 }
--- main.go
+++ main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"encoding/binary"
 	"fmt"
 	"net"
 )
 
 var connectionList []net.Conn
+
+var messageBuf []byte
 
 func main() {
 	StartServer()
@@ -38,17 +41,33 @@ func handleConnection(connection net.Conn) {
 
 	for {
 
-		buffer := make([]byte, 8096)
-		_, err := connection.Read(buffer[:])
+		header := make([]byte, 8)
+		_, err := connection.Read(header[:])
 		if err != nil {
 			fmt.Println(err)
 			connection.Close()
 			break
 		}
-		fmt.Printf("%s\n", buffer)
+
+		mlen := binary.LittleEndian.Uint32(header[4:])
+		dataBuffer := make([]byte, mlen)
+		_, err = connection.Read(dataBuffer[:])
+		if err != nil {
+			fmt.Println(err)
+			connection.Close()
+			break
+		}
+
+		messageBuf = append(messageBuf, header...)
+		messageBuf = append(messageBuf, dataBuffer...)
+
+		mtype, mlen, msg := readMessage(messageBuf)
+		messageBuf = nil
+		fmt.Printf("%d %d %s\n", mtype, mlen, msg)
 
 		for _, client := range connectionList {
-			_, err := client.Write(buffer)
+			msg := createMessage(int(mtype), msg)
+			_, err := client.Write(msg)
 			if err != nil {
 				fmt.Println(err)
 				continue
@@ -56,4 +75,22 @@ func handleConnection(connection net.Conn) {
 		}
 	}
 
+}
+
+func createMessage(mtype int, data string) []byte {
+	buf := make([]byte, 4+4+len(data))
+	binary.LittleEndian.PutUint32(buf[0:], uint32(mtype))
+	binary.LittleEndian.PutUint32(buf[4:], uint32(len(data)))
+	copy(buf[8:], []byte(data))
+
+	return buf
+
+}
+
+func readMessage(data []byte) (mtype, mlen uint32, msg string) {
+	mtype = binary.LittleEndian.Uint32(data[0:])
+	mlen = binary.LittleEndian.Uint32(data[4:])
+	msg = string(data[8:])
+
+	return mtype, mlen, msg
 }
